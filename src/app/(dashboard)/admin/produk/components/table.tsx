21824b55a0b5552d237e8e944a9f1a8252c20c4e
Moch Raditya Revanto <mochradityarevantoo@gmail.com>
2025-11-22 07:58:17 +0700

    feat: add CRUD Dashboard Product

---
+++ src/app/(dashboard)/admin/produk/components/table.tsx
@@ -0,0 +1,8 @@
+export interface MyFormData {
+  id: string;
+  name_4603829743: string;
+  name_0878515932: string;
+  name_0706064476: number;
+  name_6646786819: number;
+  name_6646786821?: number;
+}
